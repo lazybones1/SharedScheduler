c7b0ff7b286dedffab72ec16c55b0808eb62903c
Jinsan <53360337+lazybones1@users.noreply.github.com>
2021-12-22 21:29:18 +0900

    Feat: 이클립스 스프링 초기 설정
    스프링 4 설치(help - marketplace) - spring starter project 생성
     - dependencies 설정: spring web  (1개)

---
+++ sharedScheduler/src/main/java/com/ss/sharedScheduler/SharedSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.ss.sharedScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedSchedulerApplication.class, args);
+	}
+
+}
---
+++ sharedScheduler/src/test/java/com/ss/sharedScheduler/SharedSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.sharedScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
